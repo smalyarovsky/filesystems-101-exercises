e0c4579fccba07c4b41f20af43fd21d56fc8fcaf
Stepan Maliarovskii <stepan.maliarovskii@jetbrains.com>
2025-10-28 23:11:08 +0200

    00-ps rewrite everything

--- 00-ps/solution.c
+++ 00-ps/solution.c
@@ -8,89 +8,116 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-#define ARGV_MAX 8192
-#define ENV_MAX 8192
+#define ARGV_MIN 4096
 
+char ** splitToStrings(char *buffer, const size_t size) {
+	char **result = malloc((size / 2 + 1) * sizeof(char *));
+	if (!result) {
+		perror("malloc");
+		return NULL;
+	}
+	size_t j = 0;
+	for (size_t i = 0; i < size && buffer[i] != '\0'; i += strnlen(buffer + i, size - i - 1) + 1) {
+		result[j++] = buffer + i;
+	}
+	result[j] = NULL;
+	return result;
+}
+
+char * readWholeFile(const char *filename, size_t *bytes) {
+	int fd = open(filename, O_RDONLY);
+	if (fd == -1) {
+		report_error(filename, errno);
+		return NULL;
+	}
+
+	size_t size = 4096;
+	char *buffer = malloc(size);
+	if (!buffer) {
+		perror("malloc");
+		close(fd);
+		return NULL;
+	}
+
+	size_t bytesRead = 0, bytesReadTotal = 0;
+	while ((bytesRead = read(fd, buffer + bytesReadTotal, size - bytesReadTotal)) > 0) {
+		bytesReadTotal += bytesRead;
+		if (bytesReadTotal == size) {
+			size *= 2;
+			char *tmp = realloc(buffer, size);
+			if (!tmp) {
+				perror("realloc");
+				free(buffer);
+				close(fd);
+				return NULL;
+			}
+			buffer = tmp;
+		}
+	}
+	close(fd);
+	*bytes = bytesReadTotal;
+	buffer[bytesReadTotal] = '\0';
+	return buffer;
+}
 
 void ps(void) {
-	int fd;
-	DIR *pDir;
 	struct dirent *pDirent;
- 	pDir = opendir("/proc");
+ 	DIR *pDir = opendir("/proc");
 	if (pDir == NULL) {
 		report_error("/proc", errno);
 		return;
 	}
 
 	while ((pDirent = readdir(pDir)) != NULL) {
-		char *strPid = pDirent->d_name;
-
-		int isPid = 1;
-		for (int i = 0; strPid[i] != '\0'; i++) {
-			if (!isdigit(strPid[i])) {
-				isPid = 0;
-				break;
-			}
+		char *endptr;
+		const pid_t pid = (pid_t) strtol(pDirent->d_name, &endptr, 10);
+		if (*endptr != '\0') {
+			continue;
 		}
-		if (!isPid) continue;
 
-		int intPid = atoi(strPid);
+
 		char path[PATH_MAX];
 
-
-		snprintf(path, PATH_MAX, "/proc/%s/exe", strPid);
+		snprintf(path, PATH_MAX, "/proc/%s/exe", pDirent->d_name);
 		char exe[PATH_MAX];
 		if (realpath(path, exe) == NULL) {
 			report_error(path, errno);
 			continue;
 		}
 
-		snprintf(path, PATH_MAX, "/proc/%s/cmdline", strPid);
-		char cmdline[ARGV_MAX];
-		if ((fd = open(path, O_RDONLY)) == -1) {
-			report_error(path, errno);
-			continue;
-		}
-		int bytesRead = read(fd, cmdline, ARGV_MAX - 1);
-		close(fd);
-		if (bytesRead == -1) {
-			report_error(path, errno);
-			continue;
-		}
-		cmdline[bytesRead] = '\0';
-		char *argv[ARGV_MAX];
-		int j = 0;
-		for (int i = 0; i < ARGV_MAX && j < ARGV_MAX - 1 && cmdline[i] != '\0'; j++) {
-			argv[j] = cmdline + i;
-			i += strnlen(cmdline + i, ARGV_MAX - i) + 1;
-		}
-		argv[j] = NULL;
+		size_t size;
 
-
-
-		snprintf(path, PATH_MAX, "/proc/%s/environ", strPid);
-		char env[ENV_MAX];
-		if ((fd = open(path, O_RDONLY)) == -1) {
-			report_error(path, errno);
-			continue;
+		snprintf(path, PATH_MAX, "/proc/%s/cmdline", pDirent->d_name);
+		char *cmdline = readWholeFile(path, &size);
+		if (!cmdline) {
+			exit(1);
 		}
-		bytesRead = read(fd, env, ENV_MAX - 1);
-		close(fd);
-		if (bytesRead == -1) {
-			report_error(path, errno);
-			continue;
+		char **argv = splitToStrings(cmdline, size);
+		if (!argv) {
+			free(cmdline);
+			exit(1);
 		}
-		env[bytesRead] = '\0';
-
-		char *envp[ENV_MAX];
-		j = 0;
-		for (int i = 0; i < ENV_MAX && j < ENV_MAX - 1 && env[i] != '\0'; j++) {
-			envp[j] = env + i;
-			i += strnlen(env + i, ENV_MAX - i) + 1;
+		snprintf(path, PATH_MAX, "/proc/%s/environ", pDirent->d_name);
+		char *env = readWholeFile(path, &size);
+		if (!env) {
+			free(cmdline);
+			free(argv);
+			exit(1);
 		}
-		envp[j] = NULL;
+		char **envp = splitToStrings(env, size);
+		if (!envp) {
+			free(cmdline);
+			free(argv);
+			free(env);
+			exit(1);
+		}
 
-		report_process(intPid, exe, argv, envp);
+		report_process(pid, exe, argv, envp);
+
+		free(cmdline);
+		free(argv);
+		free(env);
+		free(envp);
 	}
 	closedir(pDir);
 
