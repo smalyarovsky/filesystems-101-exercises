1d71be3373bd03d89d3c2bd96c92b4308ccffbbd
Stepan Maliarovskii <stepan.maliarovskii@jetbrains.com>
2026-05-24 17:54:37 +0300

    11-grpc first attempt

--- 11-grpc/pkg/parhash/server.go
+++ 11-grpc/pkg/parhash/server.go
@@ -2,9 +2,17 @@ package parhash
 
 import (
 	"context"
+	"math/rand"
+	"net"
+	"sync"
 
 	"github.com/pkg/errors"
 	"golang.org/x/sync/semaphore"
+	"google.golang.org/grpc"
+
+	hashpb "fs101ex/pkg/gen/hashsvc"
+	parhashpb "fs101ex/pkg/gen/parhashsvc"
+	"fs101ex/pkg/workgroup"
 )
 
 type Config struct {
@@ -36,9 +44,13 @@ type Config struct {
 // Make sure that the round-robin fanout works in that case too,
 // and evenly distributes the load across backends.
 type Server struct {
-	conf Config
-
-	sem *semaphore.Weighted
+	conf  Config
+	stubs []hashpb.HashSvcClient
+	conns []*grpc.ClientConn
+	l     net.Listener
+	stop  context.CancelFunc
+	wg    sync.WaitGroup
+	sem   *semaphore.Weighted
 }
 
 func New(conf Config) *Server {
@@ -51,16 +63,71 @@ func New(conf Config) *Server {
 func (s *Server) Start(ctx context.Context) (err error) {
 	defer func() { err = errors.Wrap(err, "Start()") }()
 
-	/* implement me */
+	ctx, s.stop = context.WithCancel(ctx)
+
+	s.l, err = net.Listen("tcp", s.conf.ListenAddr)
+	if err != nil {
+		return err
+	}
+
+	srv := grpc.NewServer()
+	parhashpb.RegisterParallelHashSvcServer(srv, s)
+
+	s.stubs = make([]hashpb.HashSvcClient, len(s.conf.BackendAddrs))
+	s.conns = make([]*grpc.ClientConn, len(s.stubs))
+	for i, addr := range s.conf.BackendAddrs {
+		s.conns[i], err = grpc.Dial(addr)
+		if err != nil {
+			return err
+		}
+		s.stubs[i] = hashpb.NewHashSvcClient(s.conns[i])
+	}
+
+	s.wg.Go(func() {
+		srv.Serve(s.l)
+	})
+	s.wg.Go(func() {
+		<-ctx.Done()
+		s.l.Close()
+	})
 
 	return nil
 }
 
 func (s *Server) ListenAddr() string {
-	/* implement me */
-	return ""
+	return s.l.Addr().String()
 }
 
 func (s *Server) Stop() {
-	/* implement me */
+	s.stop()
+	s.wg.Wait()
+	for _, conn := range s.conns {
+		conn.Close()
+	}
+}
+
+func (s *Server) ParallelHash(ctx context.Context, req *parhashpb.ParHashReq) (resp *parhashpb.ParHashResp, err error) {
+	s.wg.Add(1)
+	defer s.wg.Done()
+	wg := workgroup.New(workgroup.Config{Sem: s.sem})
+	b := req.GetData()
+	resp = &parhashpb.ParHashResp{}
+	resp.Hashes = make([][]byte, len(b))
+	for i := range b {
+		i := i
+		stub := s.stubs[rand.Intn(len(s.stubs))]
+		wg.Go(ctx, func(ctx context.Context) error {
+			hashResp, err := stub.Hash(ctx, &hashpb.HashReq{Data: b[i]})
+			if err != nil {
+				return err
+			}
+			resp.Hashes[i] = hashResp.GetHash()
+			return nil
+		})
+	}
+	err = wg.Wait()
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
